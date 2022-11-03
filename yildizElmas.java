b504f1eb044e288b9985e7f277637cb95950e0cf
OZLEM OZTURKMEN <92941064+ozlemozturkmen@users.noreply.github.com>
2022-11-03 13:07:28 +0300

    yildizElmas

---
+++ yildizElmas.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+public class yildizElmas {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter any number:     ");
+        int n = sc.nextInt();
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j < (n - i); j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= (2 * i - 1); k++) {
+                System.out.print("*");
+
+            }
+            System.out.println(" ");
+
+        }
+        for (int i = 0; i <= n; i++) {
+            for (int j = n; j > (n - i); j--) {
+                System.out.print(" ");
+
+            }
+            for (int k = 1; k <= (2 * (n - i) - 1); k++) {
+                System.out.print("*");
+
+            }
+            System.out.println(" ");
+
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
