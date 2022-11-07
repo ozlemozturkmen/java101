4bc0fc8e6b192fea15e3be6cee2ea9b69df7a433
OZLEM OZTURKMEN <92941064+ozlemozturkmen@users.noreply.github.com>
2022-11-07 17:02:58 +0300

    fibonacciSerisi

---
+++ fibonacciSerisi.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class fibonacciSerisi {
+    public static void main(String[] args) {
+        int k,n1=0,n2=1,n3;
+        Scanner input=new Scanner(System.in);
+        System.out.print( " Eleman sayısını giriniz : " );
+        k= input.nextInt();
+        for(int i=2 ;i<=k;i++){
+            n3=n1+n2;
+            System.out.println(n3);
+            n1=n2;
+            n2=n3;
+            System.out.println();
+
+        }
+    }
+
+
+}
